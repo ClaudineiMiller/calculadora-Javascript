11811c8dcb2514eacad35ba20a3dbc872e9ef3b5
Claudinei Miller <claudineimiller25@gmail.com>
2021-09-19 17:06:38 -0300

    Add files via upload

---
+++ escript.js
@@ -0,0 +1,20 @@
+function calcular(tipo, valor) {
+
+    if (tipo === 'acao') {
+        if (valor === 'c') {
+            // Limpar o display
+            document.getElementById('resultado').value = ''
+        }
+
+        if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
+            document.getElementById('resultado').value += valor
+        }
+
+        if (valor === '=') {
+            var resultado_final = eval(document.getElementById('resultado').value)
+            document.getElementById('resultado').value = resultado_final
+        }
+    } else if (tipo === 'valor') {
+        document.getElementById('resultado').value += valor
+    }
+}
